3c69661349816dcdb9dfe973d838c305b3ea727f
Dawood Alnajjar <dawood.alnajjar@lnls.br>
2018-11-21 15:53:19 +0000

    Adding template files for all types of support records.

--- iocBoot/iocCRIO/st.cmd
+++ iocBoot/iocCRIO/st.cmd
@@ -14,15 +14,16 @@ CRIO_registerRecordDeviceDriver pdbbase
 crioSupSetup("/home/ABTLUS/dawood.alnajjar/work/git/crio-linux-libs/cfg/cfg.ini" , 1)
 
 ## Load record instances
-dbLoadRecords("db/devScalerCRIO.db","BL=${EPICS_HOSTNAME},SC=SCALER_DIGITAL,DTYP=CRIO Scaler,FREQ=10000000,SMN=SCALER_DIGITAL")
-dbLoadRecords ("db/devScalerCRIO.db","BL=${EPICS_HOSTNAME},SC=SCALER_ANALOG,DTYP=CRIO Scaler,FREQ=10000000,SMN=CRIO_ANALOG")
-dbLoadRecords ("db/devBICRIO.db","P=${EPICS_HOSTNAME},S=BI0,PIN=Mod3/DIO0")
-dbLoadRecords ("db/devAICRIO.db","P=${EPICS_HOSTNAME},S=AI0,PIN=Mod4/AI0")
-dbLoadRecords ("db/devAOCRIO.db","P=${EPICS_HOSTNAME},S=AO0,PIN=Mod5/AO0")
-dbLoadRecords ("db/devBOCRIO.db","P=${EPICS_HOSTNAME},S=BO0,PIN=Mod1/DIO0")
-
 
 cd ${TOP}/iocBoot/${IOC}
+
+dbLoadTemplate "bi.template"
+dbLoadTemplate "bo.template"
+dbLoadTemplate "ai.template"
+dbLoadTemplate "ao.template"
+dbLoadTemplate "scaler.template"
+
+
 iocInit
 
 dbl
