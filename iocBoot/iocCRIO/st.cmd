34ef7896ac7a06962b9facdaaa3d2c7cdae55fe6
Dawood Awny Dawood Alnajjar <dawood.alnajjar@sol11-linux.ABTLUS.org.br>
2019-03-25 08:52:26 -0300

    Fixed st.cmd paths

--- iocBoot/iocCRIO/st.cmd
+++ iocBoot/iocCRIO/st.cmd
@@ -1,6 +1,6 @@
-#!../../bin/linux-x86_64/CRIO
+#!/usr/local/epics/apps/crio-ioc/bin/linux-x86_64/CRIO
 
-< envPaths
+< /usr/local/epics/apps/crio-ioc/iocBoot/iocCRIO/envPaths
 
 cd ${TOP}
 
@@ -8,17 +8,17 @@ cd ${TOP}
 dbLoadDatabase "dbd/CRIO.dbd"
 CRIO_registerRecordDeviceDriver pdbbase
 
-crioSupSetup("/usr/local/epics/apps/config/crio/cfg.ini" , 1)
+crioSupSetup("/usr/local/epics/apps/config/crio-ioc/cfg.ini" , 1)
 
 ## Load record instances
 
 cd ${TOP}/iocBoot/${IOC}
 
-dbLoadTemplate "bi.template"
-dbLoadTemplate "bo.template"
-dbLoadTemplate "ai.template"
-dbLoadTemplate "ao.template"
-dbLoadTemplate "scaler.template"
+dbLoadTemplate "/usr/local/epics/apps/config/crio-ioc/bi.template"
+dbLoadTemplate "/usr/local/epics/apps/config/crio-ioc/bo.template"
+dbLoadTemplate "/usr/local/epics/apps/config/crio-ioc/ai.template"
+dbLoadTemplate "/usr/local/epics/apps/config/crio-ioc/ao.template"
+dbLoadTemplate "/usr/local/epics/apps/config/crio-ioc/scaler.template"
 
 
 iocInit
