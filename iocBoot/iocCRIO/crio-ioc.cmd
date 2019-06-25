9d05cfb0fe6e6080b476515e01ccbbaae369f2bd
Dawood Alnajjar <dawood.alnajjar@lnls.br>
2019-06-25 15:02:12 +0000

    Added autosave to cmd file.

--- iocBoot/iocCRIO/crio-ioc.cmd
+++ iocBoot/iocCRIO/crio-ioc.cmd
@@ -1,10 +1,11 @@
-#!/home/ABTLUS/dawood.alnajjar/work/crio-ioc/bin/linux-x86_64/CRIO
+#!/home/ABTLUS/SOL/crio-ioc/bin/linux-x86_64/CRIO
 
 
 epicsEnvSet("TOP","/usr/local/epics/apps/crio-ioc")
 epicsEnvSet("EPICS_BASE","/usr/local/epics-nfs/base/R3.15.6")
 epicsEnvSet("IOC","iocCRIO")
 epicsEnvSet("CONFIG","/usr/local/epics/apps/config/crio-ioc")
+epicsEnvSet("AUTOSAVE","/opt/autosave")
 
 
 cd ${TOP}
@@ -12,6 +13,12 @@ cd ${TOP}
 ## Register all support components
 dbLoadDatabase "dbd/CRIO.dbd"
 CRIO_registerRecordDeviceDriver pdbbase
+
+
+set_requestfile_path($(CONFIG))
+set_savefile_path($(AUTOSAVE))
+set_pass1_restoreFile("crioioc.sav", "")
+
 
 crioSupSetup("${CONFIG}/cfg.ini" , 1)
 
@@ -29,7 +36,10 @@ dbLoadTemplate "${CONFIG}/scaler.db.sub"
 dbLoadTemplate "${CONFIG}/waveform.db.sub"
 iocInit
 
+#Set initial value to a PV
+< "$(CONFIG)/init-pv.cmd"
+#dbpf SOL:CRIO4:9263A:AO0 7
+
+create_monitor_set("crioioc.req", 1, "")
+
 dbl
-
-
-
